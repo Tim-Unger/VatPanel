804e1cb27824f1ef46a683853d4eee0e649eaf85
Tim Unger <timderkuerbis@gmail.com>
2021-06-13 23:11:09 +0200

    Add project files.

---
+++ VatPanel/Clients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VatPanel
+{
+    public class Clients
+    {
+        public void VatsimClients(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+    }
+}
